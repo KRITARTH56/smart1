ffad557d846bba8ab7c6ef7951ea3232078a860b
KRITARTH56 <kritarthsign2424@gmail.com>
2021-05-10 16:27:35 +0530

    my third commit

---
+++ src/Sure.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Sure(){
+    return(
+        <div className="wrapper">
+            <div className="title">
+                Are You A Coach?
+                </div>
+                <div>
+                    <button onPress=""> YES</button>
+                    <button onPress=""> NO</button>
+                </div>
+        </div>
+    );
+}
+export default Sure;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Signup from './Signup'
+import Sure from './Sure';
 
 ReactDOM.render(
-  <Signup/>
+  <Sure/>
   ,
   document.getElementById('root')
 );
