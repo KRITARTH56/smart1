6fd17975562c110b3fedd93b5b5947e4e658e0ef
KRITARTH56 <kritarthsign2424@gmail.com>
2021-05-11 18:20:53 +0530

    my fifith commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Free from './Free';
+import Wego from './Wego';
 
 ReactDOM.render(
-  <Free/>
+  <Wego/>
   ,
   document.getElementById('root')
 );
